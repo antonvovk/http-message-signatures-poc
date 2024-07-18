e81d5c89d99b7e12789869531bd5213f18b9b89a
Anton Vovk <archwolf@protonmail.com>
2024-07-19 00:33:48 +0300

    feat: Bouncy castle hack to actually follow RFC 8017 for RSASSA-PSS

--- src/main/java/com/antonvovk/Main.java
+++ src/main/java/com/antonvovk/Main.java
@@ -3,6 +3,7 @@ package com.antonvovk;
 import lombok.SneakyThrows;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import java.security.Provider;
 import java.security.Security;
 
 import static com.antonvovk.KeyUtils.loadPrivateKeyFromFile;
@@ -10,8 +11,10 @@ import static com.antonvovk.KeyUtils.loadPublicKeyFromFile;
 
 public class Main {
 
+    public static final Provider BOUNCY_CASTLE_PROVIDER = new BouncyCastleProvider();
+
     static {
-        Security.addProvider(new BouncyCastleProvider());
+        Security.addProvider(BOUNCY_CASTLE_PROVIDER);
     }
 
     @SneakyThrows
